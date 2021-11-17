778c9538ee26a08d26095d092f55fedfccfc60d6
kacper <kacper.fernandez@gmail.com>
2021-11-17 08:36:26 +0100

    test comment

--- src/concurrentcube/Cube.java
+++ src/concurrentcube/Cube.java
@@ -5,6 +5,8 @@ import concurrentcube.Rotations.Rotation;
 import java.util.Arrays;
 import java.util.function.BiConsumer;
 
+//comment
+
 public class Cube {
     private final int size;
     private final Color[][] squares;
